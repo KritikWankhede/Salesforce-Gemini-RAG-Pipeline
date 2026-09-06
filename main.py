dd5aee6ed4e9ced4499733f59c54e8acaa09a592
Kritik <141553627+KritikWankhede@users.noreply.github.com>
2026-09-06 18:49:07 +0530

    Refactor imports and update chain module paths

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 app = FastAPI()
